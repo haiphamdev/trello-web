382e04e289174e113b51c60da25493ea0ef5a260
Hai Pham <haipham.ndev@gmail.com>
2025-03-05 20:51:53 +0700

    add dark mode select

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme} >
+    <CssVarsProvider theme={theme} >
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>,
 )
