14a2c0b778b803d67a09ba87a4706b59946cb9cf
Hai Pham <haipham.ndev@gmail.com>
2025-03-05 15:15:51 +0700

    config theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
 
   return (
     <>
+    <Typography variant='body2' color="text.secondary"  >aa</Typography>
       <h1>Hello</h1>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="contained" color='success' >Hello world</Button>
       <AddAPhotoIcon sx={{color:'red'}} />
     </>
   )
