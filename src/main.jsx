1ba24be2104cb26f0f0af022c753ced4e269fc55
Hai Pham <haipham.ndev@gmail.com>
2025-03-05 22:13:03 +0700

    basic layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import theme from './theme';
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
